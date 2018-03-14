7592c7288f2563d85809b2d2d236b60c62d4165d
Madeline Wong <madelinegwong@outlook.com>
2018-03-14 19:30:01 -0400

    created mock product data and served that as a get request response

---
+++ mocks/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        _id: 'sdfghjkl',
+        name: "Javelin (caution)",
+        price: 500,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: "asdfghjkl",
+        name: 'Albino Snail',
+        price: 3.99,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: "asdfghjklkjhgfdsdfghj",
+        name: 'Rug', 
+        price: 96.75,
+        img: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    }
+];
+
+
+
+
+module.exports = products;
